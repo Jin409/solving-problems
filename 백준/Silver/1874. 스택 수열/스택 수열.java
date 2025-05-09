566ffc612734398bc6a60c8319fbcac7ca840874
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-05-09 14:42:14 +0900

    [Silver II] Title: 스택 수열, Time: 1800 ms, Memory: 195908 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,80 +1,57 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
-        int[] targets = new int[n]; // 수열
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] numbers = new int[n];
+        List<String> result = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
-            targets[i] = Integer.parseInt(br.readLine());
+            numbers[i] = Integer.parseInt(scanner.nextLine());
         }
 
-        List<Character> ans = new ArrayList<>(List.of('+'));
-        boolean isPossible = true;
-        Stack<Integer> s = new Stack<>();
-        int currentNumber = 1; // 이미 넣은 수
-        s.push(currentNumber);
+        int number = 1;
+        int index = 0;
+        boolean isAvailable = true;
 
-        for (int target : targets) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(number);
+        result.add("+");
 
-            if (s.isEmpty()) {
-                if (currentNumber < target) {
-                    for (int i = 0; i < target - currentNumber; i++) {
-                        ans.add('+');
-                        currentNumber++;
-                        s.push(currentNumber);
-                    }
-                } else {
-                    isPossible = false;
-                    break;
-                }
+        while (index < n) {
+            if (stack.isEmpty()) {
+                number++;
+                stack.push(number);
+                result.add("+");
+                continue;
             }
 
-            Integer top = s.peek();
-            if (top == target) {
-                ans.add('-');
-                s.pop();
-            } else if (top < target) {
-                int sub = target - currentNumber;
-                for (int i = 0; i < sub; i++) {
-                    ans.add('+');
-                    currentNumber++;
-                    s.push(currentNumber);
-                }
-                s.pop();
-                ans.add('-');
-            } else {
-                while (!s.isEmpty()) {
-                    top = s.pop();
-                    ans.add('-');
-
-                    if (top == target) {
-                        break;
-                    }
-                    if (s.isEmpty()) {
-                        isPossible = false;
-                        break;
-                    }
-                }
+            int top = stack.peek();
+            if (top == numbers[index]) {
+                // top 이 해당 숫자인 경우
+                stack.pop();
+                result.add("-");
+                index++;
+                continue;
             }
-
+            if (number > numbers[index]) {
+                System.out.println("NO");
+                isAvailable = false;
+                return;
+            }
+            number++;
+            stack.push(number);
+            result.add("+");
         }
 
-        if (!isPossible) {
-            System.out.println("NO");
-        } else {
-            ans.stream().forEach(System.out::println);
+        for (String s : result) {
+            System.out.println(s);
         }
-
     }
-
 }
