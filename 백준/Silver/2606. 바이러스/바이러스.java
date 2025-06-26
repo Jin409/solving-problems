d30104130ee9cddbf8af9aa36bdc8c3d435df035
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-06-26 20:16:57 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,44 +1,50 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static boolean[] visited;
-    static boolean[][] graph;
-    static int ans;
+
+    private static int[][] map;
+    private static int[] visited;
+
+    private static int computerCount;
+
+    private static int answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        visited = new boolean[n + 1];
-        graph = new boolean[n + 1][n + 1];
-        ans = 0;
 
-        for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+        computerCount = Integer.parseInt(br.readLine());
+        map = new int[computerCount + 1][computerCount + 1]; // 1부터 사용하도록 하기
+        visited = new int[computerCount + 1];
 
-            graph[a][b] = true;
-            graph[b][a] = true;
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            int first = Integer.parseInt(s[0]);
+            int second = Integer.parseInt(s[1]);
+
+            map[first][second] = 1;
+            map[second][first] = 1;
         }
 
-        // 1번 노드가 직, 간접적으로 어떤 노드들과 연결되어 있는가
-        visited[1] = true;
         dfs(1);
-        System.out.println(ans);
+        System.out.println(answer);
     }
 
-    public static void dfs(int v) {
-        for (int i = 1; i <= n; i++) {
-            if (graph[v][i] == true && visited[i] == false) { // 연결되어 있지만 아직 방문하지 않은 경우
-                visited[i] = true;
-                ans++;
+    public static void dfs(int x) {
+        int[] connect = map[x];
+        visited[x] = 1;
+
+        for (int i = 1; i <= computerCount; i++) {
+            if (i == x) {
+                continue;
+            }
+
+            if (connect[i] == 1 && visited[i] != 1) {
+                map[x][i] = 0;
+                map[i][x] = 0;
+                answer++;
                 dfs(i);
             }
         }
