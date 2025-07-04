a815579ccbd9097fa4c736bd89784094c66be39f
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-07-04 22:42:31 +0900

    [Silver III] Title: 계단 오르기, Time: 112 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,47 +1,33 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int n;
-    static int[] stairs;
-    static int[] dp;
-
     public static void main(String[] args) throws IOException {
-        // 한번에 하나, 두개씩 오를 수 있음
-        // 연속된 세개의 계단을 모두 밟으면 안된다
-        // 마지막 도착 계단은 반드시 밟아야 한다
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine()); // 계단의 개수
-        dp = new int[n + 1];
-        stairs = new int[n + 1];
+
+        int n = Integer.parseInt(br.readLine());
+        int[] scores = new int[n + 1];
 
         for (int i = 1; i <= n; i++) {
-            stairs[i] = Integer.parseInt(br.readLine());
+            scores[i] = Integer.parseInt(br.readLine());
         }
 
-        if (n == 1) {
-            System.out.println(stairs[1]);
-        } else if (n == 2) {
-            System.out.println(stairs[1] + stairs[2]);
-        } else if (n == 3) {
-            System.out.println(Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]));
-        } else {
-            dp[1] = stairs[1];
-            dp[2] = stairs[1] + stairs[2];
-            dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+        int[] map = new int[n + 1];
+        map[1] = scores[1];
+        if (n >= 2) {
+            map[2] = map[2 - 1] + scores[2];
+        }
+        if (n >= 3) {
+            map[3] = Math.max(map[3 - 2], scores[3 - 1]) + scores[3];
+        }
 
+        if (n >= 4) {
             for (int i = 4; i <= n; i++) {
-                dp[i] = Math.max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i];
+                map[i] = Math.max(map[i - 2], map[i - 3] + scores[i - 1]) + scores[i];
             }
-
-            System.out.println(dp[n]);
-
         }
 
-
+        System.out.print(map[n]);
     }
-
 }
