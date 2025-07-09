3e555ef1997e75efb3c0dfd34be7aece82f2780a
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-07-09 21:18:49 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -4,27 +4,23 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 수를 1개 이상 사용해야 한다
-        // n 을 1,2,3 의 합으로 나타내는 방법의 수
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        int maxNum = 11;
 
-        int[] dp = new int[maxNum + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
+        int testCase = Integer.parseInt(br.readLine());
 
-        for (int i = 4; i <= maxNum; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        int[] numbers = new int[12];
+        numbers[1] = 1;
+        numbers[2] = 2;
+        numbers[3] = 4;
+
+        for (int i = 4; i <= 11; i++) {
+            numbers[i] = numbers[i - 3] + numbers[i - 2] + numbers[i - 1];
         }
 
-        for (int i = 0; i < T; i++) {
+        while (testCase > 0) {
+            testCase--;
             int n = Integer.parseInt(br.readLine());
-            System.out.println(dp[n]);
+            System.out.println(numbers[n]);
         }
-
     }
 }
