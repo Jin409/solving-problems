cbc63af45d6cb7e34af8fdbae12803f816c4d518
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-07-15 19:27:02 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -3,25 +3,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
+        int[] results = new int[n + 1];
+        results[1] = 1;
 
-        if (n == 1) {
-            System.out.println(1);
-        } else if (n == 2) {
-            System.out.println(2);
-        } else {
-            dp[1] = 1;
-            dp[2] = 2;
-
-            for (int i = 3; i <= n; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-            }
-
-            System.out.println(dp[n]);
+        if (n >= 2) {
+            results[2] = 2;
         }
 
+        if (n >= 3) {
+            results[3] = 3;
+        }
+
+        for (int i = 4; i <= n; i++) {
+            results[i] = (results[i - 1] + results[i - 2]) % 10_007;
+        }
+
+        System.out.print(results[n]);
     }
 }
