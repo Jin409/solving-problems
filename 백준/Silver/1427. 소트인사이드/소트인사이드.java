4aff5eea02e3afedfb05f01a90e09bb54240b3f5
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-08-27 20:42:03 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,24 +1,33 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
- 
-		int[] counting = new int[10];
- 
-		int n = Integer.parseInt(scanner.nextLine());
- 
-		while (n != 0) {
-			counting[n % 10]++;
-			n /= 10;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] s = br.readLine().split("");
+		int n = s.length;
+
+		int[] numbers = new int[n];
+		for(int i=0; i<n; i++){
+			numbers[i] = Integer.parseInt(s[i]);
 		}
- 
-		for (int i = 9; i >= 0; i--) {
-			while (counting[i]-- > 0) {
-				System.out.print(i);
+
+		for(int i=0; i<n; i++){
+			int maxIndex = i;
+			for(int j=i+1; j<n; j++){
+				if(numbers[maxIndex] < numbers[j]){
+					maxIndex = j;
+				}
 			}
+			// swap
+			int temp = numbers[maxIndex];
+			numbers[maxIndex] = numbers[i];
+			numbers[i] = temp;
 		}
- 
+
+		for(int i=0; i<n; i++){
+			System.out.print(numbers[i]);
+		}
 	}
 }
