a461a6b33e2e77eae17ef0842ff4d327a573374a
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-08-28 18:49:33 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,32 +1,27 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String[] s = br.readLine().split(" ");
 
-        Integer n = Integer.parseInt(br.readLine());
-        int[] times = new int[n + 1];
-        int[] result = new int[n + 1];
+		int[] numbers = new int[n];
+		for(int i=0; i<n; i++){
+			numbers[i] = Integer.parseInt(s[i]);
+		}
+		Arrays.sort(numbers);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=1; i<n; i++){
+			numbers[i] = numbers[i-1]+numbers[i];
+		}
 
-        for (int i = 1; i <= n; i++) {
-            times[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(times);
-
-        for (int i = 1; i <= n; i++) {
-            result[i] = times[i] + result[i - 1];
-        }
-
-        System.out.println(Arrays.stream(result).sum());
-    }
+		int result = 0;
+		for(int i=0; i<n; i++){
+			result += numbers[i];
+		}
+		System.out.println(result);
+	}
 }
