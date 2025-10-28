ee24b02c089268e48936374eb20c9647da635cf9
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-10-29 00:57:51 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 18.80 ms, Memory: 80.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Integer> bridge = new ArrayDeque<>();
+        for (int i = 0; i < bridge_length; i++){ bridge.offer(0); }
+
+        int time = 0;
+        int sum = 0;        
+        int idx = 0;        
+
+        while (idx < truck_weights.length) {
+            time++;
+            sum -= bridge.poll(); 
+
+            int w = truck_weights[idx];
+            if (sum + w <= weight) { 
+                bridge.offer(w);
+                sum += w;
+                idx++;
+            } else {                
+                bridge.offer(0);
+            }
+        }
+
+        return time + bridge_length;
+    }
+}
