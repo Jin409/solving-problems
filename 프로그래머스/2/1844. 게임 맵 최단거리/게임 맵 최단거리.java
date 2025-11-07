2e1025b7f44e4f6e09ef91f08301048e0dc0f9a7
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-11-07 12:04:52 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.03 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -9,28 +9,20 @@ class Solution {
         {0, 1}
     };
     
-    int n;
-    int m;
-    
     public int solution(int[][] maps) {
-        n = maps.length;
-        m = maps[0].length;
+        int answer = 0;
         
-        // 도착할 수 없는지 확인하기
-        if(!ableToReach(maps)){
-            return -1;
-        }
+        int n = maps.length;
+        int m = maps[0].length;
         
-        int[][] times = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                times[i][j] = Integer.MAX_VALUE;
-            }
-        }
+        int[][] records = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+    
+        records[0][0] = 1;
+        visited[0][0] = true;
         
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{0, 0});
-        times[0][0] = 1;
+        q.offer(new int[]{0,0});
         
         while(!q.isEmpty()){
             int[] top = q.poll();
@@ -41,39 +33,23 @@ class Solution {
             for(int[] direction : DIRECTIONS){
                 int dx = direction[0] + x;
                 int dy = direction[1] + y;
-            
-                if(inRange(dx, dy) && maps[dx][dy] == 1) {
-                    maps[dx][dy] = -1;
-                    times[dx][dy] = times[x][y]+1;
-                    q.offer(new int[]{dx, dy});
+                
+                if(inRange(dx, dy, n, m) && maps[dx][dy] == 1 && !visited[dx][dy]){
+                    visited[dx][dy] = true;
+                    records[dx][dy] = records[x][y]+1;
+                    q.offer(new int[]{dx,dy});
                 }
             }
         }
         
-        if(times[n-1][m-1] == Integer.MAX_VALUE){
+        if(!visited[n-1][m-1]){
             return -1;
         }
         
-        return times[n-1][m-1];
+        return records[n-1][m-1];
     }
     
-    private boolean inRange(int x, int y){
-        return x>=0 && y>=0 && x<n && y<m;
-    }
-    
-    private boolean ableToReach(int[][] maps){
-        int x = n-1;
-        int y = m-1;
-        
-        for(int[] direction : DIRECTIONS){
-            int dx = direction[0] + x;
-            int dy = direction[1] + y;
-            
-            if(inRange(dx, dy) && maps[dx][dy] == 1){
-                return true;
-            }
-        }
-        
-        return false;
+    private boolean inRange(int x, int y, int n, int m){
+        return x<n && x>=0 && y < m && y>=0;
     }
 }
