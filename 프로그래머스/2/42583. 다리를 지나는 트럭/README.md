4bdc9e1064aa7a69f29a0b6d5c4714c28d6fbda6
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-11-07 13:35:59 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 25.16 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?gad_source=1&gad_campaignid=22799790467&gbraid=0AAAAAC_c4nCbvfkLn3UJNTvvyzH4lKIA5&gclid=Cj0KCQiAq7HIBhDoARIsAOATDxAYRvqVM0yjsKz7feOiphDRP5OBbWCTdUBh5SHrZFwuM9WFddD8NEEaAr7cEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 18.80 ms
+메모리: 90.9 MB, 시간: 25.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 29일 00:57:46
+2025년 11월 07일 13:35:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,22 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> bridge = new ArrayDeque<>();
-        for (int i = 0; i < bridge_length; i++){ bridge.offer(0); }
+        int time = 0, sum = 0, idx = 0;
 
-        int time = 0;
-        int sum = 0;        
-        int idx = 0;        
+        // 다리 길이만큼 0으로 채워 시작 (빈 칸)
+        for (int i = 0; i < bridge_length; i++){
+            bridge.offer(0);
+        }
 
         while (idx < truck_weights.length) {
-            time++;
-            sum -= bridge.poll(); 
+            time++;                 // 1초 경과
+            sum -= bridge.poll();   // 맨 앞 칸 내려감(트럭이면 하중 감소)
 
-            int w = truck_weights[idx];
-            if (sum + w <= weight) { 
-                bridge.offer(w);
-                sum += w;
+            // 다음 트럭을 올릴 수 있으면 올리고, 아니면 빈칸(0) 유지
+            if (sum + truck_weights[idx] <= weight) {
+                bridge.offer(truck_weights[idx]);
+                sum += truck_weights[idx];
                 idx++;
-            } else {                
+            } else {
                 bridge.offer(0);
             }
         }
