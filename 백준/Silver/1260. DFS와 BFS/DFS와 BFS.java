a442192b9d917ec6ed2300cbfb9d178012089716
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2026-03-23 15:09:39 +0900

    [Silver II] Title: DFS와 BFS, Time: 308 ms, Memory: 22252 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,76 +1,67 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static boolean[][] map;
+    private static boolean[] visited;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+        int v = Integer.parseInt(input[2]); // 정점의 번호
 
-		String[] s = br.readLine().split(" ");
-		int n = Integer.parseInt(s[0]);
-		int m = Integer.parseInt(s[1]);
-		int v = Integer.parseInt(s[2]);
+        map = new boolean[n+1][n+1];
+        visited = new boolean[n+1];
 
-		List<List<Integer>> graph = new ArrayList<>();
-		for(int i=0; i<=n; i++){
-			graph.add(new ArrayList<>());
-		}
+        for(int i=0; i<m; i++){
+            input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
 
-		boolean[] visited = new boolean[n+1];
+            map[a][b] = true;
+            map[b][a] = true;
+        }
 
-		for(int i=0; i<m; i++){
-			s = br.readLine().split(" ");
+        dfs(v);
+        System.out.println();
+        bfs(v);
+    }
 
-			int a = Integer.parseInt(s[0]);
-			int b = Integer.parseInt(s[1]);
+    private static void dfs(int a){
+        System.out.print(a+" ");
+        visited[a] = true;
 
-			graph.get(a).add(b);
-			graph.get(b).add(a);
-		}
-		for(int i=1; i<=n; i++){
-			Collections.sort(graph.get(i));
-		}
+        for(int i=1; i<visited.length; i++){
+            if(visited[i] == false && (map[a][i] || map[i][a])){
+                visited[i] = true;
+                dfs(i);
+            }
+        }
+    }
 
-		StringBuffer sb = new StringBuffer();
-		visited[v] = true;
-		dfs(graph, v, visited, sb);
-		System.out.println(sb);
+    private static void bfs(int a){
+        Queue<Integer> q = new LinkedList<>();
+        visited = new boolean[map[0].length];
 
-		sb = new StringBuffer();
-		visited = new boolean[n+1];
-		visited[v] = true;
-		bfs(graph, v, visited, sb);
-		System.out.println(sb);
-	}
+        q.offer(a);
+        visited[a] = true;
 
-	public static void dfs(List<List<Integer>> graph, int index, boolean[] visited, StringBuffer sb){
-		sb.append(index+" ");
+        while(!q.isEmpty()){
+            int top = q.poll();
+            System.out.print(top+" ");
 
-		for(int i=0; i<graph.get(index).size(); i++){
-			int friend = graph.get(index).get(i);
-
-			if(!visited[friend]){
-				visited[friend] = true;
-				dfs(graph, friend, visited, sb);
-			}
-		}
-	}
-
-	public static void bfs(List<List<Integer>> graph, int index, boolean[] visited, StringBuffer sb){
-		Queue<Integer> q = new LinkedList<>();
-		q.add(index);
-
-		while(!q.isEmpty()){
-			int top = q.poll();
-			sb.append(top+" ");
-
-			for(int i=0; i<graph.get(top).size(); i++){
-				int friend = graph.get(top).get(i);
-				if(!visited[friend]){
-					q.add(friend);
-					visited[friend] = true;
-				}
-			}
-		}
-	}
+            for(int i=1; i<visited.length; i++){
+                if(i != top && visited[i] == false && (map[top][i] || map[i][top])){
+                    q.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
