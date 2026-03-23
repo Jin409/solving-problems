53fc2b76c6ba77d569e23f310d04716bb14a52e1
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2026-03-23 22:18:57 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,91 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
+
+    private static int[][] DIRECTIONS = {
+        {-1, 0},
+        {1, 0},
+        {0, 1},
+        {0, -1}
+    };
 
     private static int[][] map;
-
-    private static int n;
-
-    private static List<Integer> answer;
-
-    private static final int[] up = {-1, 0};
-    private static final int[] down = {1, 0};
-    private static final int[] left = {0, -1};
-    private static final int[] right = {0, 1};
-
-    private static final int[][] movements = {up, down, left, right};
-
+    private static int answer = 0;
+    private static List<Integer> answers = new ArrayList<>();
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         map = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0; i<n; i++){
             String[] input = br.readLine().split("");
-            for (int j = 0; j < n; j++) {
+
+            for(int j=0; j<n; j++){
                 map[i][j] = Integer.parseInt(input[j]);
             }
         }
 
-        answer = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == 0) {
-                    continue;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map[i][j] == 1){
+                    answer = 0;
+                    dfs(i, j);
+                    answers.add(answer);
                 }
-                bfs(i, j);
             }
         }
 
-        System.out.println(answer.size());
-        answer.stream().sorted().forEach(System.out::println);
+        Collections.sort(answers); 
+
+        System.out.println(answers.size());
+        for(int answer : answers){
+            System.out.println(answer);
+        }
     }
 
-    private static boolean isInRange(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < n;
+    private static boolean inRange(int x, int y){
+        return x>=0 && y >= 0 && x < map.length && y < map[0].length;
     }
 
-    private static void bfs(int x, int y) {
-        Stack<List<Integer>> stack = new Stack<>();
-        stack.add(List.of(x, y));
+    private static void dfs(int a, int b){
+        answer++;
+        map[a][b] = 0;
 
-        int count = 0;
+        for(int[] DIRECTION : DIRECTIONS){
+            int dx = a + DIRECTION[0];
+            int dy = b + DIRECTION[1];
 
-        while (!stack.isEmpty()) {
-            List<Integer> top = stack.pop();
-            Integer topX = top.get(0);
-            Integer topY = top.get(1);
-
-            if (map[topX][topY] == 0) {
+            if(!inRange(dx, dy)){
                 continue;
             }
 
-            map[topX][topY] = 0;
-            count++;
-
-            for (int[] movement : movements) {
-                int dx = movement[0] + topX;
-                int dy = movement[1] + topY;
-
-                if (!isInRange(dx, dy)) {
-                    continue;
-                }
-
-                if (map[dx][dy] == 0) {
-                    continue;
-                }
-
-                stack.add(List.of(dx, dy));
+            if(map[dx][dy] == 0){
+                continue;
             }
-        }
 
-        answer.add(count);
+            dfs(dx, dy);
+        }
     }
 }
