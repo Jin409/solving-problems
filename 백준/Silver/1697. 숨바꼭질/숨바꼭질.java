6fd1ce7a8d77baa5c86abf12a7360beeceffccc9
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2026-03-24 15:19:13 +0900

    [Silver I] Title: 숨바꼭질, Time: 184 ms, Memory: 23712 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,85 +1,47 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class Main {
+// The main method must be in a class named "Main".
+class Main {
 
+    // 방향은 -1, +1, *2
 
+    private static final int END_POINT = 100_000;
+
+    private static boolean inRange(int x){
+        return x >= 0 && x <= END_POINT;
+    }
+   
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] s = br.readLine().split(" ");
-        int n = Integer.parseInt(s[0]); // 수빈의 위치
-        int k = Integer.parseInt(s[1]); // 동생의 위치
+        String[] input = br.readLine().split(" ");
 
-        int start = Math.min(n, k); // 시작 지점
-        int end = Math.max(n, k); // 끝 지점
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
 
-        int[] map = new int[100_000 + 1];
+        int[] map = new int[END_POINT + 1];
         Arrays.fill(map, Integer.MAX_VALUE);
 
-        // 수빈이가 전진할 때
-        if (n == start) {
-            moveForward(start, map);
-        } else {
-            moveBackward(start, map);
+        map[n] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(n);
+
+        while(!q.isEmpty()){
+            int top = q.poll();
+
+            int[] candidates = new int[]{top-1, top+1, top*2};
+
+            for(int candidate : candidates){
+                if(inRange(candidate) && map[candidate] == Integer.MAX_VALUE){
+                    map[candidate] = map[top] + 1;
+                    q.offer(candidate);
+                }
+            }
         }
 
-        System.out.println(map[end]);
-    }
-
-    private static void moveBackward(int start, int[] map) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        map[start] = 0;
-
-        while (!queue.isEmpty()) {
-            Integer top = queue.poll();
-
-            if (top - 1 >= 0 && map[top - 1] == Integer.MAX_VALUE) {
-                queue.add(top - 1);
-                map[top - 1] = Math.min(map[top - 1], map[top] + 1);
-            }
-
-            if (top + 1 <= map.length - 1 && map[top + 1] == Integer.MAX_VALUE) {
-                queue.add(top + 1);
-                map[top + 1] = Math.min(map[top + 1], map[top] + 1);
-            }
-
-            if (top / 2 > 0 && map[top / 2] == Integer.MAX_VALUE) {
-                queue.add(top / 2);
-                map[top / 2] = Math.min(map[top / 2], map[top] + 1);
-            }
-
-        }
-    }
-
-    private static void moveForward(int start, int[] map) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        map[start] = 0;
-
-        while (!queue.isEmpty()) {
-            Integer top = queue.poll();
-
-            if (top - 1 >= 0 && map[top - 1] == Integer.MAX_VALUE) {
-                queue.add(top - 1);
-                map[top - 1] = Math.min(map[top - 1], map[top] + 1);
-            }
-
-            if (top + 1 <= map.length - 1 && map[top + 1] == Integer.MAX_VALUE) {
-                queue.add(top + 1);
-                map[top + 1] = Math.min(map[top + 1], map[top] + 1);
-            }
-
-            if (top * 2 <= map.length - 1 && map[top * 2] == Integer.MAX_VALUE) {
-                queue.add(top * 2);
-                map[top * 2] = Math.min(map[top * 2], map[top] + 1);
-            }
-
-        }
+        System.out.print(map[k]);
     }
 }
