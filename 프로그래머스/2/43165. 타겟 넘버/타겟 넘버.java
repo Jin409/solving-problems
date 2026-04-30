e188be7f71d9bc8e0458d1f2a023914358d4a117
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2026-04-30 12:47:50 +0900

    [level 2] Title: 타겟 넘버, Time: 4.68 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,20 +1,23 @@
 class Solution {
-    int answer = 0;
     
-    public int solution(int[] numbers, int target) {    
-        solve(numbers, target, 0, 0);
+    private int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        find(target, 0, numbers, numbers[0]);
+        find(target, 0, numbers, numbers[0] * -1);
         return answer;
     }
     
-    private void solve(int[] numbers, int target, int result, int index){
-        if(index == numbers.length){
-            if(target == result){
+    private void find(int target, int index, int[] numbers, int now){
+        
+        if(index >= numbers.length - 1){ 
+            if(now == target){
                 answer++;
-            }
+            }   
             return;
         }
-        
-        solve(numbers, target, result + (numbers[index]), index+1);
-        solve(numbers, target, result - (numbers[index]), index+1);
+    
+        find(target, index+1, numbers, now+(numbers[index+1]));
+        find(target, index+1, numbers, now+(numbers[index+1]*-1));
     }
 }
