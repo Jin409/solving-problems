81afcb52236b2cbcd20457a5f47510a9c7f43b81
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2026-05-13 13:30:56 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,9 +1,9 @@
 select a.food_type, a.rest_id, a.rest_name, a.favorites
 from rest_info a
 join (
-    select food_type, max(favorites) as max_favorite
+    select max(favorites) as max_fav, food_type
     from rest_info
     group by food_type
 ) b
-on a.food_type = b.food_type and a.favorites = b.max_favorite
-order by food_type desc;
+on a.food_type = b.food_type and b.max_fav = a.favorites
+order by food_type desc
